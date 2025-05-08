9d27a54cba0a6ad30d4da14dc0149e0bd1e40cb7
곽동욱 <kdw381@naver.com>
2025-05-08 18:59:06 +0900

    model package 형성

---
+++ src/model/BabyLion.java
@@ -0,0 +1,34 @@
+package model;
+
+public class BabyLion {
+    private String name; // 이름
+    private String selfIntro; // 자기소개
+    private int id; // 학번
+    private int good; // 성실도
+
+    public BabyLion(String name,String selfIntro,int id, int good){
+        this.name = name;
+        this.selfIntro = selfIntro;
+        this.id = id;
+        this.good = good;
+    }
+
+    // getter & setter 로 캡슐화    
+    public String getName() {return name;}
+    public String getSelfIntro() {return selfIntro;}
+    public int getId() {return id;}
+    public int getGood() {return good;}
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setSelfIntro(String selfIntro) {
+        this.selfIntro = selfIntro;
+    }
+    public void setGood(int good) {
+        this.good = good;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+}
